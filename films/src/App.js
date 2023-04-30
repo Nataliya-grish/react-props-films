b5fb1ed4235dea73f57c5f65653025e05ab762b7
Nataliya-grish <gray-wings@yandex.ru>
2023-04-30 17:04:05 +0300

    done work

---
+++ films/src/App.js
@@ -0,0 +1,8 @@
+import Stars from "./components/Stars";
+import "./App.css"
+
+function App() {
+  return <Stars count={1} />;
+}
+
+export default App;
---
+++ films/src/components/Stars.js
@@ -0,0 +1,33 @@
+import React from "react";
+import PropTypes from "prop-types";
+import shortid from "shortid";
+import Star from "./Star";
+
+function Stars({ count }) {
+  if (count < 1 || count > 5) {
+    return null;
+  }
+
+  const stars = [];
+  for (let i = 0; i < count; i++) {
+    stars.push({ id: shortid.generate(), count: i });
+  }
+
+  return (
+    <ul className="card-body-stars u-clearfix">
+      {stars.map((star) => {
+        return <Star key={star.id} />;
+      })}
+    </ul>
+  );
+}
+
+Stars.defaultProps = {
+    count: 0
+}
+
+Stars.propTypes = {
+  count: PropTypes.number,
+};
+
+export default Stars;
